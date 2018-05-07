671f0c2163e54f4518bcae1d1542c3b8b03fd9a6
Mike Lubinets <lubinetsm@yandex.ru>
2018-05-07 22:13:18 +0300

    add BTreeMap based buckets
    Signed-off-by: Mike Lubinets <lubinetsm@yandex.ru>

---
+++ src/bucket/buckets/btreemap.rs
@@ -0,0 +1,38 @@
+use std::collections::BTreeMap;
+use bucket::Bucket;
+use kv::{Key, Value};
+use smallvec::SmallVec;
+use std::marker::PhantomData;
+use std::usize;
+use std::mem;
+
+use bucket::helpers::IndexOf;
+
+pub type BTreeBucket<K, V> = BTreeMap<K, V>;
+
+impl<K: Key, V: Value> Bucket<K, V> for BTreeBucket<K, V> {
+    #[inline]
+    fn new() -> Self {
+        BTreeMap::new()
+    }
+
+    #[inline]
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        self.insert(key, value)
+    }
+
+    #[inline]
+    fn get(&self, key: K) -> Option<&V> {
+        self.get(&key)
+    }
+
+    #[inline]
+    fn get_mut(&mut self, key: K) -> Option<&mut V> {
+        self.get_mut(&key)
+    }
+
+    #[inline]
+    fn reached_max_capacity(&self) -> bool {
+        false
+    }
+}
--- src/bucket/buckets/mod.rs
+++ src/bucket/buckets/mod.rs
@@ -1,9 +1,11 @@
 pub mod vec;
 pub mod option;
 pub mod smallvec;
+pub mod btreemap;
 
 pub use self::{
     vec::VecBucket,
     option::OptionBucket,
+    btreemap::BTreeBucket,
     smallvec::*,
 };
